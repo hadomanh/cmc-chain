7bdeb7f3af7b34f449480f32781f37cf30bd7d3a
hadomanh <hadomanh99@gmail.com>
2021-12-10 08:49:24 +0700

    Fix news pagination

--- resources/views/pagination/news.blade.php
+++ resources/views/pagination/news.blade.php
@@ -5,7 +5,7 @@
                 <a href="{{ $paginator->previousPageUrl() }}" style="color: #e3007a">PREVIOUS</a>
             @endif
 
-            <a href="javascript:void(0}" class="text-muted">{{'Page ' . $paginator->currentPage() . ' of ' . $paginator->total()}}</a>
+            <a href="javascript:void(0}" class="text-muted">{{'Page ' . $paginator->currentPage() . ' of ' . $paginator->lastPage()}}</a>
             
             @if ($paginator->hasMorePages())
                 <a href="{{ $paginator->url($paginator->lastPage()) }}" style="color: #e3007a">NEXT</a>
